e0fd429ebd87830b8ba5954911252a3ece42fb14
sanych85 <50615912+sanych85@users.noreply.github.com>
2020-11-21 01:20:32 +0300

    form updated

--- src/CommonComponents/CustomFormElem/CustomFormElem.js
+++ src/CommonComponents/CustomFormElem/CustomFormElem.js
@@ -1,10 +1,11 @@
 import React from "react";
-const CustomFormElem = ({ tagName, type, placeholder }) => {
+const CustomFormElem = ({ tagName, type, placeholder,handleFormElem, name }) => {
   const detectCustomFormElem = () => {
+    console.log(handleFormElem)
     if (tagName === "textarea") {
-      return <textarea type={type} placeholder={placeholder} />;
+      return <textarea type={type} placeholder={placeholder} name = {name} onChange = {(e)=>handleFormElem(e)} />;
     } else {
-      return <input type={type} placeholder={placeholder} />;
+      return <input type={type} placeholder={placeholder} name = {name} onChange = {(e)=>handleFormElem(e)} />;
     }
   };
 
--- src/CommonComponents/Forms/NewPersonForm.js
+++ src/CommonComponents/Forms/NewPersonForm.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import CustomFormElem from "../CustomFormElem"
 const NewPersonForm = () => {
+    const [personForm, handleForm] =useState({name:'', description:'', year:''});
+    
+    const handleFormElem = (e)=> {
+        const elemName =e.target.name;
+        const value = e.target.value;
+        console.log(e.target.name)
+        
+        handleForm({...personForm, [elemName]:value})
+    }
+    console.log(personForm)
     return (
+
         <form action="">
-            <CustomFormElem type = "string" placeholder="name"></CustomFormElem>
-            <CustomFormElem type = "number" placeholder = "date"></CustomFormElem>
-            <CustomFormElem tagName = "textarea" placeholder = "enter description"></CustomFormElem>
+            <CustomFormElem type = "string" name="name" placeholder="name" value = {personForm.name} handleFormElem = {handleFormElem}></CustomFormElem>
+            <CustomFormElem type = "number" name="date" placeholder = "date" value = {personForm.date} handleFormElem = {handleFormElem}></CustomFormElem>
+            <CustomFormElem tagName = "textarea" name="description" placeholder = "enter description" value = {personForm.description} handleFormElem = {handleFormElem}></CustomFormElem>
         </form>
     );
 }
